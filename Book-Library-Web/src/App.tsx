7fe26d9e207946372f2c3867ed1c9ccdb2a9057b
Jonas Rafael <jonasrafael97@hotmail.com>
2023-02-10 09:44:50 -0300

    Add table results

--- Book-Library-Web/src/App.tsx
+++ Book-Library-Web/src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css";
-import { Button, Container, TextField } from "@mui/material";
+import {
+  Button,
+  Container,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
 import styled from "@emotion/styled";
-import { useState, BaseSyntheticEvent } from "react";
+import { useState, BaseSyntheticEvent, useEffect } from "react";
 import axios from "axios";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import Paper from "@mui/material/Paper";
+import Pagination from "@mui/material/Pagination";
 
 type Book = {
   id: number;
@@ -17,30 +29,20 @@ type Book = {
   category: string;
 };
 
+type Page = {
+  totalOfItems: number;
+  itemsPerPage: number;
+  currentPage: number;
+  totalPages: number;
+  items: Book[];
+};
+
 function App() {
   const [inputValue, setInputValue] = useState<string>("");
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [row, setRow] = useState<Book>();
+  const [rows, setRows] = useState<Book[]>([]);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const columns: GridColDef[] = [
-    { field: "title", headerName: "Title", width: 70 },
-    { field: "author", headerName: "Author", width: 130 },
-    { field: "type", headerName: "Type", width: 130 },
-    {
-      field: "ISBN",
-      headerName: "ISBN",
-      width: 90,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) =>
-        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-    },
-  ];
   const getUrl = () => {
     if (inputValue) {
       return `https://localhost:7182/v1/Books/${inputValue}/${currentPage}`;
@@ -48,15 +50,33 @@ function App() {
     return `https://localhost:7182/v1/Books/${currentPage}`;
   };
 
+  useEffect(() => {
+    const req = async () => {
+      await makeRequest();
+    };
+    req();
+  }, [currentPage]);
+
+  const makeRequest = async () => {
+    const response = await axios.get<Page>(getUrl());
+    setTotalPages(response.data.totalPages);
+    if (response.data) setRows(response.data.items);
+  };
+
   const handleSubmit = async (evt: any) => {
     evt.preventDefault();
+    await makeRequest();
+  };
 
-    const response = await axios.get<Book[]>(getUrl());
-    if (response.data) setRow(response.data);
+  const handleChangePage = async (
+    evt: React.ChangeEvent<unknown>,
+    page: number
+  ) => {
+    setCurrentPage(page);
   };
 
   return (
-    <Container maxWidth="sm">
+    <Container>
       <Card>
         <form onSubmit={handleSubmit}>
           <StyledTitle>Royal Library</StyledTitle>
@@ -77,12 +97,42 @@ function App() {
         </form>
       </Card>
       <Card>
-        <DataGrid
-          rows={row}
-          columns={columns}
-          pageSize={6}
-          rowsPerPageOptions={[5]}
-        />
+        {rows.length > 0 && (
+          <>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TitleCell align="left">Title</TitleCell>
+                    <TitleCell align="left">Author</TitleCell>
+                    <TitleCell align="left">Type</TitleCell>
+                    <TitleCell align="left">ISBN</TitleCell>
+                    <TitleCell align="left">Category</TitleCell>
+                    <TitleCell align="left">Available Copies</TitleCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows?.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <TableCell align="left">{row.title}</TableCell>
+                      <TableCell align="left">{`${row.firstName} ${row.lastName}`}</TableCell>
+                      <TableCell align="left">{row.type}</TableCell>
+                      <TableCell align="left">{row.isbn}</TableCell>
+                      <TableCell align="left">{row.category}</TableCell>
+                      <TableCell align="left">
+                        {row.copiesInUse}/{row.totalCopies}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <Pagination count={totalPages} onChange={handleChangePage} />
+          </>
+        )}
       </Card>
     </Container>
   );
@@ -90,7 +140,7 @@ function App() {
 
 const Card = styled.div`
   height: auto;
-  width: 500px;
+  width: 1280px;
   padding: 16px;
   border: 2px solid white;
   border-radius: 4px;
@@ -110,6 +160,11 @@ const StyledButton = styled(Button)`
   :hover {
     background-color: #4c4d4e;
   }
+`;
+
+const TitleCell = styled(TableCell)`
+  font-size: 16px;
+  font-weight: bold;
 `;
 
 const StyledTextField = styled(TextField)`
